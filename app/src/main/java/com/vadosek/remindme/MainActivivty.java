a1ca2b5d69d3503deedb7a3452c879076850e759
Vladislav Akulinin <vadson40@gmail.ru>
2018-10-25 10:48:42 +0300

    Code

--- app/src/main/java/com/vadosek/remindme/MainActivivty.java
+++ app/src/main/java/com/vadosek/remindme/MainActivivty.java
@@ -1,8 +1,18 @@
 package com.vadosek.remindme;
 
-/**
- * Created by Vladislav on 25.10.2018.
- */
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivivty {
+import com.example.vladislav.remindme.R;
+
+
+public class MainActivivty extends AppCompatActivity{
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
